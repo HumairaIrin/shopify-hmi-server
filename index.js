4dd4e10295f9d9b9e6395b60314737781b3abb25
HumairaIrin <humairamuntaha24@gmail.com>
2022-11-28 02:26:22 +0600

    database connected with server

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tgametl.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    const productsCollection = client.db('resaleMarket').collection('allProducts');
+    try {
+
+    }
+    finally { }
+}
+run().catch(console.log(error));
+
+
 app.get('/', (req, res) => {
     res.send('resale market server is running');
+})
+
+app.listen(port, () => {
+    console.log(`resale server running on port ${port}`);
 })

9e8a3cc854cbc39f23a271ac44f46b1af0388527
HumairaIrin <humairamuntaha24@gmail.com>
2022-12-02 18:36:59 +0600

    product delete by seller completed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -118,7 +118,7 @@ async function run() {
             res.send(result);
         })
 
-        //updating seller verification status 
+        //updating seller's verification status 
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
             const filter = { email: email };
@@ -130,6 +130,14 @@ async function run() {
                 }
             }
             const result = await usersCollection.updateOne(filter, updatedUser, option);
+            res.send(result);
+        })
+
+        // product delete by seller
+        app.delete('/seller/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productsCollection.deleteOne(query);
             res.send(result);
         })
 

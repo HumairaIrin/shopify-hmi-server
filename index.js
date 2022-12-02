9caa7564d5b4ebd3b59f4ec70f1bae8f3afa0b83
HumairaIrin <humairamuntaha24@gmail.com>
2022-12-02 06:56:04 +0600

    reading users account status method added

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ async function run() {
             res.send(categories);
         })
 
+        // checking users accountType is buyer or not
         app.get('/user/buyer/:email', async (req, res) => {
             const email = req.params.email;
             const query = { email };
@@ -32,6 +33,7 @@ async function run() {
             res.send({ isBuyer: user?.accountType === 'buyer' })
         })
 
+        // checking users accountType is seller or not
         app.get('/user/seller/:email', async (req, res) => {
             const email = req.params.email;
             const query = { email };
@@ -39,6 +41,7 @@ async function run() {
             res.send({ isSeller: user?.accountType === 'seller' })
         })
 
+        // getting sellers own products
         app.get('/myProducts', async (req, res) => {
             const userEmail = req.query.email;
             const query = { sellersEmail: userEmail }
@@ -46,6 +49,7 @@ async function run() {
             res.send(myProducts)
         })
 
+        // reading the category products by searching with categoryName
         app.get('/products/:categoryName', async (req, res) => {
             const name = req.params.categoryName;
             const query = { categoryName: name };
@@ -53,6 +57,7 @@ async function run() {
             res.send(products);
         })
 
+        // taking all the buyers info to set on admin's dashboard
         app.get('/users/buyers', async (req, res) => {
             const type = req.query.type;
             const query = { accountType: type };
@@ -60,18 +65,27 @@ async function run() {
             res.send(buyers);
         })
 
+        // taking all the sellers info to set on admin's dashboard
         app.get('/users/sellers', async (req, res) => {
             const type = req.query.type;
             const query = { accountType: type };
             const sellers = await usersCollection.find(query).toArray();
             res.send(sellers);
         })
-
+        // checking if the user accountType is admin or not 
         app.get('/user/admin/:email', async (req, res) => {
             const email = req.params.email;
             const query = { email };
             const user = await usersCollection.findOne(query);
             res.send({ isAdmin: user?.accountType === 'admin' });
+        })
+
+        // checking seller is verified or not
+        app.get('/user/seller/status/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email };
+            const user = await usersCollection.findOne(query);
+            res.send({ isVerified: user?.accountStatus === 'verified' });
         })
 
         app.post('/user', async (req, res) => {
@@ -80,6 +94,7 @@ async function run() {
             res.send(result)
         })
 
+        // taking booking from buyer and inserting in the bookingColection
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
             const query = {
@@ -96,12 +111,14 @@ async function run() {
             res.send(result);
         })
 
+        // adding product to db(added by seller)
         app.post('/addProduct', async (req, res) => {
             const product = req.body;
             const result = await productsCollection.insertOne(product);
             res.send(result);
         })
 
+        //updating seller verification status 
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
             const filter = { email: email };
@@ -116,6 +133,7 @@ async function run() {
             res.send(result);
         })
 
+        // deleting user from db (deleted by admin)
         app.delete('/user/:email', async (req, res) => {
             const email = req.params.email;
             const query = { email };
